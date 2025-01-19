4d75eea751857c61620a3fa666805e564782c163
Meena Liju <68983909+MeenakshyLiju@users.noreply.github.com>
2025-01-19 18:14:51 +0000

    Add files via upload

---
+++ Flask Dashboard/main.py
@@ -0,0 +1,22 @@
+# import os
+
+# # Define folder and file structure
+# project_structure = {
+#     "data": ["final_data.csv"],
+#     "static": ["style.css", "script.js"],
+#     "templates": ["index.html", "sentiment.html", "entity.html", "comparison.html"],
+#     "": ["app.py", "requirements.txt"]
+# }
+
+# # Create folders and files
+# for folder, files in project_structure.items():
+#     folder_path = os.path.join(os.getcwd(), folder)
+#     os.makedirs(folder_path, exist_ok=True)
+#     for file in files:
+#         file_path = os.path.join(folder_path, file)
+#         if not os.path.exists(file_path):
+#             open(file_path, 'w').close()
+
+# print("✅ Project structure created successfully!")
+
+print("hello")
